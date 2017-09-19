ab77d1a17e1e0495a65d18914743ddfaef89bd9d
Vítor Miguel <vitomiguel@gmail.com>
2017-09-19 17:58:35 -0300

    1

--- server.js
+++ server.js
@@ -11,22 +11,18 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/comprar', function(req, res) {
-    res.send('comprar');
-})
-
 app.post('/capture', function(req, res){
     console.log(req.body);
 
-    // var amount = req.body.transaction.amount;
-    // var token = req.body.transaction.id;
+    var amount = req.body.transaction.amount;
+    var token = req.body.transaction.id;
 
-    // console.log('\n amount: ' + amount + ' id: ' + token);
+    console.log('\n amount: ' + amount + ' id: ' + token);
     
-    // pagarme.client
-    //     .connect({ api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU' })
-    //     .then(client => client.transactions.capture({ id: token, amount: amount }))
-    //     .catch(error => console.error(error));
+    pagarme.client
+        .connect({ api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU' })
+        .then(client => client.transactions.capture({ id: token, amount: amount }))
+        .catch(error => console.error(error));
 });
 
 app.listen(80, function(){console.log('server running');});
