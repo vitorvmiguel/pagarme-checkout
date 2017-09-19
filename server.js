232c9957029be2dd73450996ca8572a718d8de2e
Vítor Miguel <vitomiguel@gmail.com>
2017-09-19 16:18:12 -0300

    req.body

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.get('/', function(req, res) {
 });
 
 app.post('/capture', function(req, res){
-    var amount = req.body.amount;
-    var token = req.body.token;
-    console.log('amount: ' + amount + ' token: ' + token);
+
+    console.log(req.body);
     
     pagarme.client.connect({ api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU' })
     .then(client => client.transactions.capture({ id: token, amount: amount }));
