95a5550d7e348b853411f993745b153cccc08d0b
Vítor Miguel <vitomiguel@gmail.com>
2017-09-19 16:55:15 -0300

    req.body change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
