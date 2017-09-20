fa08847e6811e02448e6a1602d6ac304b26ca26c
Vítor Miguel <vitomiguel@gmail.com>
2017-09-20 13:14:02 -0300

    1

--- server.js
+++ server.js
@@ -13,40 +13,42 @@ app.get('/', function(req, res) {
 
 app.post('/comprar', function(req, res) { 
 
-    var print;
+    console.log('REQ.BODY: ' + JSON.stringify(req.body));
 
-    if (req.body.token) {
-        console.log('*********** TOKEN: ' + JSON.stringify(req.body));
+    // var print;
 
-        print = '<h3>Token: ' + req.body.token + '</h3>\n';
+    // if (req.body.token) {
+    //     console.log('*********** TOKEN: ' + JSON.stringify(req.body));
+
+    //     print = '<h3>Token: ' + req.body.token + '</h3>\n';
         
-    } else {
-        console.log('*********** CARD_HASH: ' + JSON.stringify(req.body.pagarme));
+    // } else {
+    //     console.log('*********** CARD_HASH: ' + JSON.stringify(req.body.pagarme));
 
-        print = '<h3>Card_hash: ' + req.body.pagarme.card_hash + '</3>';
+    //     print = '<h3>Card_hash: ' + req.body.pagarme.card_hash + '</3>';
 
 
-        var transaction = JSON.stringify(req.body.pagarme);
+    //     var transaction = JSON.stringify(req.body.pagarme);
         
-        pagarme.client
-        .connect({api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU'})
-        .then(client => client.transactions.create({transaction}))
-        .catch(error => error.response.errors.map(console.log));
-    }
+    //     pagarme.client
+    //     .connect({api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU'})
+    //     .then(client => client.transactions.create({transaction}))
+    //     .catch(error => error.response.errors.map(console.log));
+    // }
 
-    res.send(print);
+    // res.send(print);
 
 });
 
 app.post('/capture', function(req, res){
 
-    var amount = req.body.transaction.amount;
-    var token = req.body.transaction.id;
+    // var amount = req.body.transaction.amount;
+    // var token = req.body.transaction.id;
     
-    pagarme.client
-    .connect({ api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU' })
-    .then(client => client.transactions.capture({ id: token, amount: amount }))
-    .catch(error => console.error(error));
+    // pagarme.client
+    // .connect({ api_key: 'ak_test_mvofz5xg6lezCy0HrZVHE2stg6oudU' })
+    // .then(client => client.transactions.capture({ id: token, amount: amount }))
+    // .catch(error => console.error(error));
 });
 
 app.listen(80, function(){console.log('server running');});
